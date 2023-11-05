13cc473bfbda28e231d695e0fb058b97e1bca0e1
webbaipiao <2244713679@qq.com>
2023-11-05 16:53:18 +0800

    feat: delete dist dir

--- utils/getFile.js
+++ utils/getFile.js
@@ -1,34 +1,37 @@
-import fs from "fs";
+import fs from 'fs';
 
-const findFile = async (filePath = "src") => {
-  try {
-    const data = await fs.readdirSync(`./${filePath}`);
-    const result = [];
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i];
-      const targetFilePath = `${filePath}/${item}`;
-      // 如果式src/index.ts 直接跳过
-      if (targetFilePath === "src/index.ts") {
-        continue;
-      }
-      // 如果式以.ts文件为结尾，则认为是文件
-      if (item.endsWith(".ts")) {
-        result.push(targetFilePath);
-      } else {
-        // 否则就是文件夹
-        const r = await findFile(`${filePath}/${item}`);
-        result.push(r);
-      }
-    }
-    return result;
-  } catch (err) {
-    return [];
-  }
+const findFile = async (filePath = 'src') => {
+	try {
+		const data = await fs.readdirSync(`./${filePath}`);
+		const result = [];
+		for (let i = 0; i < data.length; i++) {
+			const item = data[i];
+			const targetFilePath = `${filePath}/${item}`;
+			// 如果式src/index.ts 直接跳过
+			if (targetFilePath === 'src/index.ts') {
+				continue;
+			}
+			// 如果式以.ts文件为结尾，则认为是文件
+			if (item.endsWith('.ts')) {
+				result.push(targetFilePath);
+			} else {
+				// 否则就是文件夹
+				// eslint-disable-next-line no-await-in-loop
+				const r = await findFile(`${filePath}/${item}`);
+				result.push(r);
+			}
+		}
+		return result;
+	} catch (err) {
+		return [];
+	}
 };
 
 const filePath = await findFile();
-let utilsFilePath = filePath.flat(Infinity);
+const utilsFilePath = filePath.flat(Infinity);
 
-const writeFileData = `const utilsFilePath = ${JSON.stringify(utilsFilePath)};export default utilsFilePath `;
+const writeFileData = `const utilsFilePath = ${JSON.stringify(
+	utilsFilePath,
+)};export default utilsFilePath `;
 
-fs.writeFile("./utils/file.js", writeFileData, () => {});
+fs.writeFile('./utils/file.js', writeFileData, () => {});
